c38b5c237a45fdf1556957b5f8b4b81a4cc0b2a1
Natanael Tan <nonnoxer@gmail.com>
2019-07-29 10:43:26 +0800

    0.0.1

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Website creator: konikal (flask)
 2. py -m konikal create app
 3. ???
 4. Profit!
+
+helpful links
+* https://github.com/BillMills/pythonPackageLesson
+* https://realpython.com/command-line-interfaces-python-argparse/
+* https://stackoverflow.com/questions/17073688/how-to-use-argparse-subparsers-correctly
--- konikal.py
+++ konikal.py
@@ -1,5 +1,29 @@
-def create():
-    print("Yes I am")
+import os
+import argparse
+
+def create(path):
+    if os.path.exists(path):
+        os.chdir(path)
+        app = open("app.py", "w+")
+        app.write("print(\"Hello world!\")")
+    else:
+        path_list = path.split("/")
+        for i in path_list:
+            try:
+                os.mkdir(i)
+            except FileExistsError:
+                pass
+            except FileNotFoundError:
+                print("Invalid path")
+                exit()
+            os.chdir(i)
+        app = open("app.py", "w+")
+        app.write("print(\"Hello world!\")")
+        
 
 if __name__ == "__main__":
-    create()
+    parser = argparse.ArgumentParser()
+    create_parser = parser.add_subparsers().add_parser("create")
+    create_parser.add_argument("path", nargs="?", default=".", metavar="path", type=str)
+    args = parser.parse_args()
+    create(vars(args)["path"])
